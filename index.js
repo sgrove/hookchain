2541ef3b778c9da8d2c9d1686a972aa4156ea5df
Sean Grove <s@Seans-MacBook-Pro-2.local>
2021-04-23 18:01:03 -0700

    more info

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 const { fetch } = require("fetch-ponyfill")({});
-
-const callChain = async (params) => {
-  const appId = params.appId;
-  delete params.appId;
+// "ExecuteChainMutation_httpRouteTest"
+const callChain = async (appId, operationName, params) => {
+  console.log("IncomingHttpRequest payload:");
+  console.log(JSON.stringify(params, null, 2));
   const response = await fetch(
     `https://serve.onegraph.com/graphql?app_id=${appId}`,
     {
@@ -12,7 +12,7 @@ const callChain = async (params) => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         doc_id: params.chainId,
-        operationName: "ExecuteChainMutation_httpRouteTest",
+        operationName: operationName,
         variables: params,
       }),
     }
@@ -38,14 +38,15 @@ const forwardResonse = async ({ forward }) => {
   return response.text();
 };
 
-app.get("/app/:appId/chain/:chainId", async function (req, res) {
-  const { appId, chainId } = req.params;
-  let incomingHeaders = Object.entries(req.headers);
+app.get("/app/:appId/chain/:chainId/:operationName", async function (req, res) {
+  const { appId, chainId, operationName } = req.params;
+  let incomingHeaders = { ...req.headers };
+  console.log("Params: ", req.params);
+  delete incomingHeaders.cookie;
   let body = req.body || "";
   let jsonBody = JSON.parse(req.body || "null");
   let metadata = {};
   const params = {
-    appId: appId,
     chainId: chainId,
     headers: incomingHeaders,
     path: req.path,
@@ -56,11 +57,13 @@ app.get("/app/:appId/chain/:chainId", async function (req, res) {
     metadata: metadata,
   };
 
-  const result = await callChain(params);
+  const result = await callChain(appId, operationName, params);
 
   const results = result?.data?.oneGraph?.executeChain?.results;
 
-  console.log(results);
+  console.log(result);
+
+  console.log("Plucked results: ", result);
 
   const argumentDependencies = results
     ?.find((i) => i.request.id === "OutgoingHttpResponse")
